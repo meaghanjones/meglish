2132b9beaa006e40a33749aca501e93d9c587ee8
Meaghan Jones <meaghan.m.jones@gmail.com>
2016-09-09 09:42:57 -0700

    set up intial files for meglish project: including database

---
+++ app/models/course.rb
@@ -0,0 +1,5 @@
+class Course < ActiveRecord::Base
+  validates :name, :presence => true
+  validates :description, :presence => true
+
+end
---
+++ db/migrate/20160909161409_create_courses.rb
@@ -0,0 +1,10 @@
+class CreateCourses < ActiveRecord::Migration[5.0]
+  def change
+    create_table :courses do |t|
+      t.column :name, :string
+      t.column :description, :string
+
+      t.timestamps
+    end
+  end
+end

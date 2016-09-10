2887147c84df91a896c615c7355e9ed28fdd0dbd
Meaghan Jones <meaghan.m.jones@gmail.com>
2016-09-09 17:09:04 -0700

    add the color palatte

---
+++ spec/features/add_lesson_pages_spec.rb
@@ -0,0 +1,37 @@
+require 'rails_helper'
+
+describe "the add a lesson process" do
+  it "adds a new lesson" do
+    visit courses_path
+    click_link 'New Course'
+    fill_in 'Name', :with => 'English Slang'
+    fill_in 'Description', :with => 'The best class on slang to ever exist'
+    click_on 'Create Course'
+    click_on 'English Slang'
+    click_on 'New Lesson'
+    fill_in 'Name', with: => 'Important Cultural Aspects of Interviewing'
+    fill_in 'Age', with: => 'adults'
+    fill_in 'description', with: => 'This lesson gives you a video to watch and homework to complete.'
+    fill_in 'video', with: => 'www.youtube.com'
+    fill_in 'aim', with: => 'to be good at interviewing'
+    fill_in 'skills', with: => 'speaking'
+    fill_in 'time', with: => '1'
+    expect(page).to have_content 'Lessons'
+  end
+
+  it "gives error when no name is entered" do
+    visit courses_path
+    click_link 'New Course'
+    fill_in 'Name', :with => 'English Slang'
+    fill_in 'Description', :with => 'The best class on slang to ever exist'
+    click_on 'Create Course'
+    click_on 'English Slang'
+    click_on 'New Lesson'
+    fill_in 'description', with: => 'This lesson gives you a video to watch and homework to complete.'
+    fill_in 'video', with: => 'www.youtube.com'
+    fill_in 'aim', with: => 'to be good at interviewing'
+    fill_in 'skills', with: => 'speaking'
+    fill_in 'time', with: => '1'
+    expect(page).to have_content 'errors'
+  end
+end

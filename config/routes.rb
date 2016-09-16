08acb5c208c678f4e0456acec149aa4bfde1ed38
meaghanjones <meaghan.m.jones@gmail.com>
2016-09-15 19:33:14 -0700

    add dynamic titles to all current html pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,10 @@
 Rails.application.routes.draw do
   get 'static_pages/home'
-
   get 'static_pages/about'
 
   devise_for :users
   root :to => 'courses#index'
-  
+
   resources :courses do
     resources :lessons
   end

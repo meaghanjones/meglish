35b702e47ef8f7e36793f88190c2f94ebc5eccd8
meaghanjones <meaghan.m.jones@gmail.com>
2016-09-15 20:09:18 -0700

    add nav bar with a link to sign out of the session

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -1,4 +1,5 @@
 class LessonsController < ApplicationController
+
   def new
     @course = Course.find(params[:course_id])
     @lesson = @course.lessons.new
--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,12 @@ Rails.application.routes.draw do
   get 'static_pages/home'
   get 'static_pages/about'
 
-  devise_for :users
   root :to => 'courses#index'
+
+  devise_for :users
+  resources :users do
+    resources :courses
+  end
 
   resources :courses do
     resources :lessons

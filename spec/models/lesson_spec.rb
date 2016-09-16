5dc5ad268e072f1046faccb0d1bd630dc52bb88a
Meaghan Jones <meaghan.m.jones@gmail.com>
2016-09-16 14:22:52 -0700

    fix lesson spec so it is passing

--- spec/models/lesson_spec.rb
+++ spec/models/lesson_spec.rb
@@ -2,7 +2,6 @@ require 'rails_helper'
 
 describe Lesson do
   it { should validate_presence_of :name}
-  it { should validate_presence_of :age}
   it { should validate_presence_of :description}
   it { should validate_presence_of :time}
   it { should belong_to :course}

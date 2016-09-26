12f63fd8289b74d348df023ca7861b9d071e8370
meaghanjones <meaghan.m.jones@gmail.com>
2016-09-26 15:35:30 -0700

    fix add lesson integration spec so add a new lesson passes

--- spec/features/add_lesson_pages_spec.rb
+++ spec/features/add_lesson_pages_spec.rb
@@ -3,16 +3,9 @@ require 'rails_helper'
 describe "the add a lesson process" do
   it "adds a new lesson" do
     user = FactoryGirl.create(:user)
+    course = FactoryGirl.create(:course)
     login_as(user, :scope => :user, :run_callbacks => false)
-    visit root_path
-    click_on 'New Course'
-    fill_in 'Name', :with => 'English for Hiking'
-    fill_in 'Description', :with => 'Next time you need to hike youll know all the words'
-    fill_in 'Level', :with => 'Intermediate'
-    fill_in 'Age', :with => 'All Ages'
-    fill_in 'Skill', :with => 'Speaking'
-    click_on "Create Course"
-    click_button 'Start Learning!'
+    visit course_path(course)
     click_on 'New Lesson'
     fill_in 'Name', :with => 'Packing list for hiking'
     fill_in 'Description', :with => 'This lesson gives you a video to watch and homework to complete.'
@@ -20,7 +13,7 @@ describe "the add a lesson process" do
     fill_in 'Skills', :with => 'speaking'
     fill_in 'Time', :with => '1'
     fill_in 'Content', :with => '#Hi!'
-    expect(page).to have_content 'English for Hiking'
+    expect(page).to have_content course.name
   end
 
   it "gives error when no name is entered" do

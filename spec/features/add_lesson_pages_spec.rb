81c360473e491e34d8c916785701e4a537e899f7
meaghanjones <meaghan.m.jones@gmail.com>
2016-09-26 20:29:39 -0700

    edit specs so the add lessons spec is now passing correctly

--- spec/features/add_lesson_pages_spec.rb
+++ spec/features/add_lesson_pages_spec.rb
@@ -20,12 +20,13 @@ describe "the add a lesson process" do
     user = FactoryGirl.create(:user)
     login_as(user, :scope => :user, :run_callbacks => false)
     course = FactoryGirl.create(:course)
-    visit courses_path
+    visit course_path(course)
     click_on 'New Lesson'
-    fill_in 'description', :with => 'This lesson gives you a video to watch and homework to complete.'
-    fill_in 'video', :with => 'www.youtube.com'
-    fill_in 'skills', :with => 'speaking'
-    fill_in 'time', :with => '1'
+    fill_in 'Description', :with => 'This lesson gives you a video to watch and homework to complete.'
+    fill_in 'Video', :with => 'www.youtube.com'
+    fill_in 'Skills', :with => 'speaking'
+    fill_in 'Time', :with => '1'
+    click_on 'Save'
     expect(page).to have_content 'errors'
   end
 end

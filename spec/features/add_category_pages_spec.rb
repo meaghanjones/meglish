c701d2e192c8f8d9204eaf8614ba3f676dcc1265
meaghanjones <meaghan.m.jones@gmail.com>
2016-09-28 16:43:23 -0700

    add a passing spec for ajax in the categories form

--- spec/features/add_category_pages_spec.rb
+++ spec/features/add_category_pages_spec.rb
@@ -1,13 +1,13 @@
 require 'rails_helper'
 
-describe "the add a category process" do
+describe "the add a category process", js: true do
   it "adds a new category" do
     user = FactoryGirl.create(:user)
     login_as(user, :scope => :user, :run_callbacks => false)
     visit root_path
     click_on "New Course"
     click_on 'add category'
-    fill_in 'Name', :with => 'Business English'
+    fill_in 'category_name', :with => 'Business English'
     click_on 'Create Category'
     expect(page).to have_content 'Business English'
   end

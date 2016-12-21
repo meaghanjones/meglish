5592eee50eff7a994a6ec0e9e1dfdde93ac4581d
meaghanjones <meaghan.m.jones@gmail.com>
2016-12-21 18:31:30 -0200

    remove before action authenticate_user on courses controller

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -1,5 +1,5 @@
 class CoursesController < ApplicationController
-  before_action :authenticate_user!
+  # before_action :authenticate_user!
   before_action :admin_user,     except: [:index, :show]
   # load_and_authorize_resource :through => :current_user
 
